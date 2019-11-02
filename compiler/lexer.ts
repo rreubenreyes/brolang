0238ebeb4b84aacfff9c6da1de65df53fccf5f8d
Reuben Reyes <reuben.a.reyes@gmail.com>
2019-11-02 00:57:29 -0700

    fix: Ditch factory functions, distinguish tokens by functionality

--- compiler/lexer.ts
+++ compiler/lexer.ts
@@ -1,116 +1,160 @@
-/*******************/
-/* Base Definition */
-/*******************/
-enum Species {
-    Keyword = '@@brolang_keyword',
-    Literal = '@@brolang_literal',
-    Operator ='@@brolang_operator'
-}
-
+/*************************/
+/* Base Token Definition */
+/*************************/
 type GrammarPredicate = (expr: string) => boolean;
 
 type Token = {
     readonly name: string;
-    readonly species: Species;
     readonly predicate: GrammarPredicate;
 }
 
-function factory<S extends Species>(species: S) {
-    return function tokenFactory<T extends Token>(
-        args: Omit<T, 'species'> & { [key: string]: T[keyof T] }
-    ) {
-        const { name, predicate, ...etc } = args;
-
-        return {
-            species,
-            name,
-            predicate,
-            ...etc,
-        }
-    }
-}
-
-const Literal = factory(Species.Literal);
-const Keyword = factory(Species.Keyword);
-const Operator = factory(Species.Operator);
-
-/************/
-/* Literals */
-/************/
+/**********************/
+/* Literal Definition */
+/**********************/
 type Literal = Token & {
-    species: Species.Literal;
+    readonly compilesTo: 'number' | 'string';
 }
 
-const NUMBER = Literal({
-    name: 'number',
-    predicate: s => /[0-9]/.test(s),
-});
-
-const STRING = Literal({
-    name: 'string',
-    predicate: s => /^["'].*["']$/.test(s),
-});
-
-/************/
-/* Keywords */
-/************/
-type Keyword = Token & {
-    readonly species: Species.Keyword;
-}
-
-const PROGRAM = Keyword({
-    name: 'program',
-    predicate: s => /program/i.test(s),
-});
-
-const PERIODIZATION = Keyword({
-    name: 'periodization',
-    predicate: s => /periodization/i.test(s),
-});
-
-const PERIOD = Keyword({
-    name: 'period',
-    predicate: s => /period/i.test(s),
-});
-
-const WEEK = Keyword({
-    name: 'week',
-    predicate: s => /week/i.test(s),
-});
-
-const DAY = Keyword({
-    name: 'day',
-    predicate: s => /day/i.test(s),
-})
-
-/*************/
-/* Operators */
-/*************/
-type Operator = Token & {
-    readonly species: Species.Operator;
-}
-
+/********************/
+/* Block Definition */
+/********************/
 enum EnclosingBound {
     Opening = 1,
     Closing = -1,
 }
 
-type OpeningBound = Operator & {
+type OpeningBound = Token & {
     readonly delimiting: EnclosingBound.Opening;
 }
 
-type ClosingBound = Operator & {
-    readonly species: Species.Operator;
+type ClosingBound = Token & {
     readonly delimiting: EnclosingBound.Closing;
 }
 
-type BoundingPair = {
+type Block = {
     readonly opening: OpeningBound;
     readonly closing: ClosingBound;
 }
 
-const ARRAY_OPEN: OpeningBound = Operator({
+/***********************/
+/* Operator Definition */
+/***********************/
+// type UnaryOperator = Token & {
+//     readonly operands: (t: Token) => any;
+// }
+
+type BinaryOperator = Token & {
+    eval<T extends Token>(a: T, b: T): any;
+}
+
+type TernaryOperator = Token & {
+    eval<T extends Token>(
+        a: T,
+        b: T,
+        c: T,
+    ): any;
+}
+
+/**********************/
+/* Keyword Definition */
+/**********************/
+type Keyword = Token & {
+    readonly boundBy?: Block[];
+}
+
+
+/************/
+/* Literals */
+/************/
+const NUMBER: Literal = {
+    name: 'number',
+    compilesTo: 'number',
+    predicate: /[0-9]+/.test,
+}
+
+const STRING: Literal = {
+    name: 'string',
+    compilesTo: 'string',
+    predicate: /^["'].*["']$/.test,
+};
+
+/**********/
+/* Blocks */
+/**********/
+const ARRAY_OPEN: OpeningBound = {
     name: 'array_open',
-    predicate: s => /\[/.test(s),
+    predicate: /\(/.test,
     delimiting: EnclosingBound.Opening
-});
+};
+
+const ARRAY_CLOSE: ClosingBound = {
+    name: 'array_open',
+    predicate: /\)/.test,
+    delimiting: EnclosingBound.Closing
+};
+
+const ARRAY: Block = {
+    opening: ARRAY_OPEN,
+    closing: ARRAY_CLOSE,
+}
+
+const HEADING_OPEN: OpeningBound = {
+    name: 'array_open',
+    predicate: /\[/.test,
+    delimiting: EnclosingBound.Opening
+};
+
+const HEADING_CLOSE: ClosingBound = {
+    name: 'array_open',
+    predicate: /\]/.test,
+    delimiting: EnclosingBound.Closing
+};
+
+const HEADING: Block = {
+    opening: HEADING_OPEN,
+    closing: HEADING_CLOSE,
+}
+
+/*************/
+/* Operators */
+/*************/
+// const ASSIGNMENT: BinaryOperator = {
+//     name: 'assignment',
+//     predicate: /:/.test,
+//     eval (a: Keyword, b: Literal) => {
+//         return { [a.name]: b }
+//     }
+// };
+
+/************/
+/* Keywords */
+/************/
+const PROGRAM: Keyword = {
+    name: 'program',
+    predicate: /program/i.test,
+    boundBy: [HEADING],
+};
+
+const PERIODIZATION: Keyword = {
+    name: 'periodization',
+    predicate: /periodization/i.test,
+    boundBy: [HEADING],
+};
+
+const PERIOD: Keyword = {
+    name: 'period',
+    predicate: /period/i.test,
+    boundBy: [HEADING],
+};
+
+const WEEK: Keyword = {
+    name: 'week',
+    predicate: /week/i.test,
+    boundBy: [HEADING],
+};
+
+const DAY: Keyword = {
+    name: 'day',
+    predicate: /day/i.test,
+    boundBy: [HEADING],
+};
